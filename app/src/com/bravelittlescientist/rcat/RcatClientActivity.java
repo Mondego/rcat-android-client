2214a97955a30db065e00c25031b9cf569161348
Eugenia Gabrielova <genia.likes.science@gmail.com>
2013-01-17 02:10:02 -0800

    Base RCAT client application

---
+++ app/src/com/bravelittlescientist/rcat/RcatClientActivity.java
@@ -0,0 +1,15 @@
+package com.bravelittlescientist.rcat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RcatClientActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
